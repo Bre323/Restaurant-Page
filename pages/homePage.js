ad24945f759b76cf8867278ae24cc2541584dcab
Breno Rocha <brenopenarocha2@gmail.com>
2024-05-02 18:31:25 -0300

    webpack configs, pages folder and generateHomePage function

---
+++ pages/homePage.js
@@ -0,0 +1,51 @@
+import peezaPizzaImg from  '../src/assets/peeza-pizza-image.jpeg';
+
+const generateHomePage = () => {
+    let content = document.getElementById('content');
+    let homeDiv = document.createElement('div');
+    homeDiv.classList.add('home');
+
+
+    let description = document.createElement('div');
+    description.classList.add('description');
+
+    let descriptionImage = document.createElement('img');
+    descriptionImage.src = peezaPizzaImg;
+    descriptionImage.alt = 'Peeza Pizza Image';
+
+    let descriptionText = document.createElement('p');
+    descriptionText.innerText = 'Peeza Pizza is the best pizza out there. Lot of different flavors that make you want to eat lots of pizza. Check our menu and enjoy.';
+
+    let hours = document.createElement('div');
+    hours.classList.add('hours');
+
+    let hoursTitle = document.createElement('h2');
+    hoursTitle.innerText = 'Hours';
+
+    let hoursList = document.createElement('li');
+    let monday = document.createElement('ul');
+    let tuesday = document.createElement('ul');
+    let wednesday = document.createElement('ul');
+    let thursday = document.createElement('ul');
+    let friday = document.createElement('ul');
+    let saturday = document.createElement('ul');
+    let sunday = document.createElement('ul');
+    monday.innerText = 'Monday: 6am to 8pm';
+    tuesday.innerText = 'Monday: 6am to 8pm';
+    wednesday.innerText = 'Monday: 6am to 8pm';
+    thursday.innerText = 'Monday: 6am to 10pm';
+    friday.innerText = 'Monday: 6am to 10pm';
+    saturday.innerText = 'Monday: 6am to 6pm';
+    sunday.innerText = 'Monday: 6am to 3pm';
+
+
+    hoursList.append(monday, tuesday, wednesday, thursday, friday, saturday, sunday);
+    hours.append(hoursTitle, hoursList);
+    description.append(descriptionImage, descriptionText);
+    homeDiv.append(description, hours);
+    content.appendChild(homeDiv);
+
+    return content;
+}
+
+export default generateHomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Website is working");
+import generateHomePage from "../pages/homePage";
+import './style.css';
+
+let homeButton = document.getElementById('home-button');
+let menuButton = document.getElementById('menu-button');
+let contactButton = document.getElementById('contact-button');
+let contentDiv = document.getElementById('content');
+
+homeButton.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    generateHomePage();
+});
+
+menuButton.addEventListener("click", () => console.log("This is the menu button!"));
+contactButton.addEventListener("click", () => console.log("This is the contact button!"));
